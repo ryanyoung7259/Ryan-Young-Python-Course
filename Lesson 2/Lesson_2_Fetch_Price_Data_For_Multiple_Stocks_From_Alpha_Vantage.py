86093d427982ebc2e620643113444833c757faa9
ryanyoung7259 <ryanyoung7259@gmail.com>
2020-06-03 13:59:31 -0400

    Update Lesson_2_Fetch_Price_Data_For_Multiple_Stocks_From_Alpha_Vantage.py

--- Lesson 2/Lesson_2_Fetch_Price_Data_For_Multiple_Stocks_From_Alpha_Vantage.py
+++ Lesson 2/Lesson_2_Fetch_Price_Data_For_Multiple_Stocks_From_Alpha_Vantage.py
@@ -1,7 +1,14 @@
+#Generic Trading Algorithm
+
 #Lesson #2: Fetch price data for multiple stocks from Alpha Vantage
+
 import pandas as pd
 import numpy as np
 from alpha_vantage.timeseries import TimeSeries
+
+#Uncomment the following two lines if you want to see the entire DataFrame
+#pd.set_option('display.max_columns', 999)
+#pd.set_option('display.max_rows', 50000)
 
 av_gmail_credentials = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
 
@@ -12,7 +19,6 @@ high_list = []
 low_list = []
 close_list = []
 volume_list = []
-dividend_list = []
 
 for ticker in symbols_list:
     alpha_vantage_tuple = av_gmail_credentials.get_daily_adjusted(symbol=ticker, outputsize='full')
@@ -22,25 +28,25 @@ for ticker in symbols_list:
     alpha_vantage_df3 = alpha_vantage_df2.reset_index()
 
     #Make sure the index of dataframe alpha_vantage_df3 is type datetime
-    alpha_vantage_df3['date'] = pd.to_datetime(alpha_vantage_df3['date'])
+    alpha_vantage_df3['date'] = pd.to_datetime(alpha_vantage_df3['date'], infer_datetime_format=True)
 
-    alpha_vantage_df3.columns = ['Date', 'open', 'high', 'low', 'close', 'volume']
+    alpha_vantage_df3.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
 
     alpha_vantage_df4 = alpha_vantage_df3.set_index('Date')
 
     alpha_vantage_df5 = alpha_vantage_df4.sort_index(ascending=True)
 
-    alpha_vantage_df6 = alpha_vantage_df5.add_prefix(ticker + ' ')
+    alpha_vantage_df6 = alpha_vantage_df5.add_prefix(ticker + '_')
 
-    alpha_vantage_df_open_historical_data = alpha_vantage_df6[ticker + ' open']
+    alpha_vantage_df_open_historical_data = alpha_vantage_df6[ticker + '_Open']
 
-    alpha_vantage_df_high_historical_data = alpha_vantage_df6[ticker + ' high']
+    alpha_vantage_df_high_historical_data = alpha_vantage_df6[ticker + '_High']
 
-    alpha_vantage_df_low_historical_data = alpha_vantage_df6[ticker + ' low']
+    alpha_vantage_df_low_historical_data = alpha_vantage_df6[ticker + '_Low']
 
-    alpha_vantage_df_close_historical_data = alpha_vantage_df6[ticker + ' close']
+    alpha_vantage_df_close_historical_data = alpha_vantage_df6[ticker + '_Close']
 
-    alpha_vantage_df_volume_historical_data = alpha_vantage_df6[ticker + ' volume']
+    alpha_vantage_df_volume_historical_data = alpha_vantage_df6[ticker + '_Volume']
 
     #Rename Series to ticker symbol
     alpha_vantage_df_open_historical_data.rename(ticker)
@@ -60,6 +66,8 @@ df_high_historical = pd.concat(high_list, axis=1, sort=True)
 df_low_historical = pd.concat(low_list, axis=1, sort=True)
 df_close_historical = pd.concat(close_list, axis=1, sort=True)
 df_volume_historical = pd.concat(volume_list, axis=1, sort=True)
+
+df_close_historical.dropna(axis=0, how='any', inplace=True)
 
 print(df_close_historical)
 print(df_open_historical)
